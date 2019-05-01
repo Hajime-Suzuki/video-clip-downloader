327b545ce0ebaabfeaa8f171a8a6e8dbe9e1a383
Hajime-Suzuki <hajime.doublebass@gmail.com>
2019-05-02 00:07:04 +0200

    show headers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 
-res = requests.get('https://realpython.com/python-requests/')
+res = requests.get('https://pwa-test-1234.netlify.com/')
 
 with open('test.html', 'w') as file:
     file.write(res.text)
+    print(res.headers)
